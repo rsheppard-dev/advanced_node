11120b87e181d64626f1acf853fff6d9b22f2f3a
rsheppard83 <rsheppard83@gmail.com>
2021-06-25 22:03:13 +0100

    Authentication Strategies

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local')
 const ObjectID = require('mongodb').ObjectID
 
 const app = express();
@@ -27,6 +28,16 @@ app.use(passport.session());
 myDB(async client => {
   const myDatabase = await client.db('database').collection('users')
 
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDatabase.findOne({ username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`)
+      if (err) { return done(err) }
+      if (!user) { return done(null, false) }
+      if (password !== user.password) { return done(null, false) }
+      return done(null, user)
+    })
+  }))
+
   app.route('/').get((req, res) => {
     res.render('pug', {
       title: 'Connected to Database',
@@ -40,10 +51,10 @@ myDB(async client => {
   
   passport.deserializeUser((id, done) => {
     myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, null)
+      done(null, doc)
     })
   })
-  
+
 }).catch(e => {
   app.route('/').get((req, res) => {
     res.render('pug', {
