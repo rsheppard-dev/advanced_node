881e7fa25918ca35ef6f474645969855e606511f
rsheppard83 <rsheppard83@gmail.com>
2021-06-25 21:11:27 +0100

    Serialization of a User Object

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict';
+require('dotenv').config({ path: './config/.env' })
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID
 
 const app = express();
 app.set('view engine', 'pug');
@@ -26,6 +28,16 @@ app.route('/').get((req, res) => {
   // Change the response to render the Pug template
   res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
 });
+
+passport.serializeUser((user, done) => {
+  done(null, user._id)
+})
+
+passport.deserializeUser((id, done) => {
+  // myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null)
+  // })
+})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT);
