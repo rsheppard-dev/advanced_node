6095a2cb08956f4d0352d63efc20d196c76f6dc2
rsheppard83 <rsheppard83@gmail.com>
2021-06-25 22:28:41 +0100

    How to Use Passport Strategies

--- server.js
+++ server.js
@@ -41,8 +41,19 @@ myDB(async client => {
   app.route('/').get((req, res) => {
     res.render('pug', {
       title: 'Connected to Database',
-      message: 'Please login'
+      message: 'Please login',
+      showLogin: true
     })
+  })
+
+  app.route('/login').post(passport.authenticate('local', {
+    failureRedirect: '/'
+  }), (req, res) => {
+    res.redirect('/profile')
+  })   
+  
+  app.route('/profile').get((req, res) => {
+    res.render('pug/profile')
   })
 
   passport.serializeUser((user, done) => {
