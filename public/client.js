c91a47ab1cd179e17238179ff9a32f4d33927af7
rsheppard83 <rsheppard83@gmail.com>
2021-06-27 15:30:15 +0100

    updated user socket

--- public/client.js
+++ public/client.js
@@ -1,8 +1,11 @@
 //* global io */
 let socket = io()
 
-socket.on('user count', data => {
-  console.log(data)
+socket.on('user', data => {
+  $('#num-users').text(data.currentUsers + ' users online')
+  let message = data.name +
+  data.connected ? ' has joined the chat.' : ' has left the chat.'
+  $('#messages').append($('<li>').html('<b>' + message + '</b>'))
 })
 
 $(document).ready(function () {
--- server.js
+++ server.js
@@ -51,11 +51,19 @@ myDB(async client => {
   io.on('connection', socket => {
     console.log(`user ${socket.request.user.name} connected`)
     ++currentUsers
-    io.emit('user count', currentUsers)
+    io.emit('user', {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true
+    })
 
     socket.on('disconnect', () => {
       --currentUsers
-      io.emit('user count', currentUsers)
+      io.emit('user', {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false
+      })
     })
   })
 
