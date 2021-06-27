adad638b0a9f874572df26d41cf982619519ac89
rsheppard83 <rsheppard83@gmail.com>
2021-06-27 07:07:10 +0100

    create new middleware

--- middleware/ensureAuthenticated.js
+++ middleware/ensureAuthenticated.js
@@ -1,5 +1,7 @@
 const ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) return next()
+    if (req.isAuthenticated()) {
+        return next()
+    }
     res.redirect('/')
 }
 
