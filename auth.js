71c5fa87e9603bad25a66ff1bfe210f4dc897330
rsheppard83 <rsheppard83@gmail.com>
2021-06-27 12:47:41 +0100

    updated auth to work with github oauth

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
+require('dotenv').config({ path: './config/.env' })
 const passport = require('passport')
 const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local')
+const GitHubStrategy = require('passport-github')
 
 module.exports = function (app, myDatabase) {
     passport.use(new LocalStrategy((username, password, done) => {
@@ -11,6 +13,14 @@ module.exports = function (app, myDatabase) {
             if (!bcrypt.compareSync(password, user.password)) { return done(null, false) }
             return done(null, user)
         })
+    }))
+
+    passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://replit.com/@rsheppard/auth/github/callback'
+    }, (accessToken, refreshToken, profile, cb) => {
+        console.log(profile)
     }))
 
     passport.serializeUser((user, done) => {
